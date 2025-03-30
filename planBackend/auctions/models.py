a082ed1e221d7b48780e95eb1e8d73907fe6a06e
miguelangel-huamani <202204727@alu.comillas.edu>
2025-03-30 19:03:38 +0200

    project "planBackend" created and models.py implemented

---
+++ planBackend/auctions/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, blank=False, unique=True)
+
+    class Meta:
+        ordering=('id',)
+
+    def __str__(self):
+        return self.name
+    
+
+class Auction(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    closing_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='auctions/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    stock = models.IntegerField(validators=[MinValueValidator(0)])
+    rating = models.DecimalField(max_digits=3, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    category = models.ForeignKey(Category, related_name='auctions',on_delete=models.CASCADE)
+    brand = models.CharField(max_length=100)
+
+    class Meta:
+        ordering=('id',)
+
+    def __str__(self):
+        return self.title
+    
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    creation_date = models.DateTimeField(auto_now_add=True)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(Auction, related_name='bids', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-amount',)
+
+    def __str__(self):
+        return self.amount
+
