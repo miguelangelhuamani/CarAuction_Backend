24729027d9726361939b91f8185fbed18d71bca0
miguelangel-huamani <202204727@alu.comillas.edu>
2025-04-01 11:12:57 +0200

    urls.py (auctions and app's)

---
+++ planBackend/auctions/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import CategoryListCreate, CategoryRetrieveUpdateDestroy, AuctionListCreate, AuctionRetrieveUpdateDestroy, BidListCreate, BidRetrieveUpdateDestroy
+
+
+app_name="auctions"
+urlpatterns = [
+    #Auctions URLs
+    path('', AuctionListCreate.as_view(), name='auction-list-create'),
+    path('<int:pk>/', AuctionRetrieveUpdateDestroy.as_view(), name='auction-detail'),
+
+    #Categories URLs
+    path('categories/', CategoryListCreate.as_view(), name='category-list-create'),
+    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroy.as_view(), name='category-detail'),
+
+    #Bis URLs
+    path('<int:auction_id>/bids/', BidListCreate.as_view(), name='bid-list-create'),
+    path('<int:auction_id>/bids/<int:pk>/', BidRetrieveUpdateDestroy.as_view(), name='bid-detail'),
+]
