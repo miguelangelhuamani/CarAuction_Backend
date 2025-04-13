6e71809d89e08d866c98e25f23547f0c97f95781
Ibinarriaga8 <202206789@alu.comillas.edu>
2025-04-13 23:54:53 +0200

    Remove merge conflict markers and clean up permissions, serializers, URLs, and settings files.

--- planBackend/auctions/permissions.py
+++ planBackend/auctions/permissions.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrAdmin(BasePermission):
@@ -12,23 +11,3 @@ class IsOwnerOrAdmin(BasePermission):
             return True
         # Permitir si el usuario es el creador o es administrador
         return obj.auctioneer == request.user or request.user.is_staff
-=======
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-
-class IsOwnerOrAdmin(BasePermission):
-    """
-    Permite editar/eliminar una subasta solo si el usuario es el propietario
-    o es administrador. Cualquiera puede consultar (GET).
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Permitir acceso de lectura a cualquier usuario (GET, HEAD, OPTIONS)
-        if request.method in SAFE_METHODS:
-            return True
-        
-        # Permitir si el usuario es el creador o es administrador
-        return obj.auctioneer == request.user or request.user.is_staff
-    
-
-#IMPORTANTE: request.user ES EL USUARIO AUTENTICADO
->>>>>>> main
--- planBackend/auctions/urls.py
+++ planBackend/auctions/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 from .views import CategoryListCreate, CategoryRetrieveUpdateDestroy, AuctionListCreate, AuctionRetrieveUpdateDestroy, BidListCreate, BidRetrieveUpdateDestroy, UserAuctionListView, UserBidListView
 
@@ -19,25 +18,4 @@ urlpatterns = [
 
     path('users/', UserAuctionListView.as_view(), name='action-from-users'),
     path('bids/users/', UserBidListView.as_view(), name='bids-from-users'),
-=======
-from django.urls import path
-from .views import CategoryListCreate, CategoryRetrieveUpdateDestroy, AuctionListCreate, AuctionRetrieveUpdateDestroy, BidListCreate, BidRetrieveUpdateDestroy, UserAuctionListView
-
-
-app_name="auctions"
-urlpatterns = [
-    #Auctions URLs
-    path('', AuctionListCreate.as_view(), name='auction-list-create'),
-    path('<int:pk>/', AuctionRetrieveUpdateDestroy.as_view(), name='auction-detail'),
-
-    #Categories URLs
-    path('categories/', CategoryListCreate.as_view(), name='category-list-create'),
-    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroy.as_view(), name='category-detail'),
-
-    #Bis URLs
-    path('<int:auction_id>/bids/', BidListCreate.as_view(), name='bid-list-create'),
-    path('<int:auction_id>/bids/<int:pk>/', BidRetrieveUpdateDestroy.as_view(), name='bid-detail'),
-
-    path('users/', UserAuctionListView.as_view(), name='action-from-users'),
->>>>>>> main
 ]
