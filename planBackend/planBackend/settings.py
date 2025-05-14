88fcbb21729ca78a5cc97bbc5fad20633af7d896
Ibinarriaga8 <202206789@alu.comillas.edu>
2025-05-14 12:38:04 +0200

    Conflicot BBDD

--- planBackend/planBackend/settings.py
+++ planBackend/planBackend/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = "planBackend.wsgi.application"
 
 load_dotenv()
 DATABASES = {
-'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
